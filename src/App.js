4bea1265aa48523ecd176d8eb97f06449c87ecb7
Diego <diegoprudencioo@gmail.com>
2023-02-02 09:30:41 -0300

    router and pages criation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="contact">Contato</Link></li>
+            <li><Link to="company">Empresa</Link></li>
+            <li><Link to="newProject">Novo Projeto</Link></li>
+      </ul>
+
+      <Routes>
+        <Route exact path='/' element={<Home /> }></Route>
+        <Route path='/company' element={<Company />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/newProject' element={<NewProject />}></Route>
+      </Routes>
+
+      <p>Footer</p>
+    </Router>
   );
 }
 
