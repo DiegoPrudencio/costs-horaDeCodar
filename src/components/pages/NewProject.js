d9293ee63c2aa461fd093f7dec9ce5bd8be22491
Diego <diegoprudencioo@gmail.com>
2023-02-15 20:41:06 -0300

    :books: docs: main: update NewProject component and create its CSS

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,8 +1,16 @@
+import styles from './NewProject.module.css'
+
 function NewProject() {
     
-    return (<>
-        <h1>New Project</h1>
-    </>)
+    return (
+    
+        <div className={styles.newProject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para adicionar serviços</p>
+            <p>Formuário</p>
+        </div>
+
+    )
 
 }
 
