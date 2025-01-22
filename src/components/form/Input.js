10bf4d15fb84ebb6c4f99f4e57dd76f37d7d7d76
Diego <diegoprudencioo@gmail.com>
2025-01-22 00:13:00 -0300

    updated project

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,6 +1,6 @@
 import styles from './Input.module.css'
 
-function Input({ type, text, name, placeholder, handlerOnChange, value }) {
+function Input({ type, text, name, placeholder, handleOnChange, value }) {
 
     return (
 
@@ -11,7 +11,7 @@ function Input({ type, text, name, placeholder, handlerOnChange, value }) {
                 name={name}
                 id={name}
                 placeholder={placeholder}
-                onChange={handlerOnChange}
+                onChange={handleOnChange}
                 value={value}
             />
         </div>
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,14 +1,38 @@
 import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
+import {useNavigate} from 'react-router-dom'
 
 function NewProject() {
+
+    const navigate = useNavigate()
     
+    function createPost(project) {
+        // initalize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch("http://localhost:5000/projects", {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        }) 
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                navigate('/projects', { state: { message: 'Projeto enviado com sucesso!' }})
+            })
+            .catch((err) => console.log(err))
+
+    }
+
     return (
     
         <div className={styles.newProject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para adicionar serviços</p>
-            <ProjectForm btnText="Criar projeto"/>
+            <ProjectForm handleSubmit={createPost} btnText="Criar projeto"/>
         </div>
 
     )
